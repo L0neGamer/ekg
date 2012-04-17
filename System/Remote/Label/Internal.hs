42357a50ea3aaa48123148c17007111ff0fcf4dd
Iustin Pop <iusty@k1024.org>
2012-04-17 22:21:30 +0200

    Add a Label metric type
    This is used to hold Text values, which are free-form and can be used
    for (e.g.) hostname, command line, etc.
    
    I've used Text, but I'm not sure whether String would make more sense
    for (what are supposed to be) such small values.

---
+++ System/Remote/Label/Internal.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_HADDOCK not-home #-}
+module System.Remote.Label.Internal
+    (
+      Label(..)
+    , new
+    , read
+    ) where
+
+import Data.IORef (IORef, newIORef, readIORef)
+import qualified Data.Text as T
+import Prelude hiding (read)
+
+-- | A mutable, integer-valued label.
+newtype Label = C { unC :: IORef T.Text }
+
+-- | Create a new, null initialized, label.
+new :: IO Label
+new = C `fmap` newIORef T.empty
+
+read :: Label -> IO T.Text
+read = readIORef . unC
