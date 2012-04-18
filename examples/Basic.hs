2cf463e0cf052cbbb133946140258e5aa166f31a
Johan Tibell <johan.tibell@gmail.com>
2012-04-17 19:06:32 -0700

    Extend example with labels

--- examples/Basic.hs
+++ examples/Basic.hs
@@ -7,6 +7,7 @@ module Main where
 import Control.Concurrent
 import Control.Exception
 import qualified System.Remote.Counter as Counter
+import qualified System.Remote.Label as Label
 import System.Remote.Monitoring
 
 mean :: Fractional a => [a] -> a
@@ -16,6 +17,8 @@ main :: IO ()
 main = do
     handle <- forkServer "localhost" 8000
     counter <- getCounter "iterations" handle
+    label <- getLabel "args" handle
+    Label.set label "some text string"
     let loop n = do
             evaluate $ mean [1..n]
             threadDelay 2000
