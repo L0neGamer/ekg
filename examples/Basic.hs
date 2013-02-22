a5436c7e2eaeb0fca2abfb9abc83b5daa4aa68fb
Johan Tibell <johan.tibell@gmail.com>
2013-02-22 09:42:43 -0800

    Change basic example to not blow the stack
    It was using sum from the Prelude which is implemented using a
    non-strict left fold, causing it to blow the stack.

--- examples/Basic.hs
+++ examples/Basic.hs
@@ -6,12 +6,17 @@ module Main where
 
 import Control.Concurrent
 import Control.Exception
+import Data.List
 import qualified System.Remote.Counter as Counter
 import qualified System.Remote.Label as Label
 import System.Remote.Monitoring
 
+-- 'sum' is using a non-strict lazy fold and will blow the stack.
+sum' :: Num a => [a] -> a
+sum' = foldl' (+) 0
+
 mean :: Fractional a => [a] -> a
-mean xs = sum xs / fromIntegral (length xs)
+mean xs = sum' xs / fromIntegral (length xs)
 
 main :: IO ()
 main = do
